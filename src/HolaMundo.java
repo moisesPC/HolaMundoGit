41e071fb4282e484dbcc5d6e03ffd17a23508c94
moisesPC <moisesapc64@gmail.com>
2024-03-07 23:41:29 +0000

    Cambio en consola

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo SEGUNDA MODIFICACION\n");
+		System.out.println("Otra linea editada en VIM\n");
 	}
 
 }
